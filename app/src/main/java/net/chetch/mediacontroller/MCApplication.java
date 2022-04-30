23098c6dddbd749a43c3d17b54adf9bf22377024
billchetch <45006134+billchetch@users.noreply.github.com>
2022-04-30 23:30:31 +0800

    s

--- app/src/main/java/net/chetch/mediacontroller/MCApplication.java
+++ app/src/main/java/net/chetch/mediacontroller/MCApplication.java
@@ -5,7 +5,7 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 import net.chetch.appframework.ChetchApplication;
-//import net.chetch.webservices.network.NetworkRepository;
+import net.chetch.webservices.network.NetworkRepository;
 
 public class MCApplication extends ChetchApplication {
     @Override
