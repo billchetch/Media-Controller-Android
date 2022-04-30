2b2d31647c6db9d4b2348c13fedc6a8f646ac1c6
billchetch <45006134+billchetch@users.noreply.github.com>
2022-04-30 23:24:52 +0800

    s

--- app/src/main/java/net/chetch/mediacontroller/MCApplication.java
+++ app/src/main/java/net/chetch/mediacontroller/MCApplication.java
@@ -8,5 +8,19 @@ import net.chetch.appframework.ChetchApplication;
 //import net.chetch.webservices.network.NetworkRepository;
 
 public class MCApplication extends ChetchApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        try{
+            String apiBaseURL = sharedPref.getString("api_base_url", null);
+            NetworkRepository.getInstance().setAPIBaseURL(apiBaseURL);
+
+        } catch (Exception e){
+            Log.e("ERApplication", e.getMessage());
+        }
+    }
 }
